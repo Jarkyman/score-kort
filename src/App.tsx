46327f3931c846568a770e672e2787e8ec6072e4
Jackie Jensen <Jackiehartvig@hotmail.com>
2026-04-01 23:41:14 +0200

    feat: add beta welcome popup

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,20 @@ import HomePage from "./pages/HomePage";
 import ClubListPage from "./pages/ClubListPage";
 import ClubDetailPage from "./pages/ClubDetailPage";
 import RequestPage from "./pages/RequestPage";
+import BetaPopup from "./components/BetaPopup";
 
 export default function App() {
     return (
-        <Routes>
-            <Route element={<Layout />}>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/klubber" element={<ClubListPage />} />
-                <Route path="/klub/:clubId" element={<ClubDetailPage />} />
-                <Route path="/indberetning" element={<RequestPage />} />
-            </Route>
-        </Routes>
+        <>
+            <BetaPopup />
+            <Routes>
+                <Route element={<Layout />}>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/klubber" element={<ClubListPage />} />
+                    <Route path="/klub/:clubId" element={<ClubDetailPage />} />
+                    <Route path="/indberetning" element={<RequestPage />} />
+                </Route>
+            </Routes>
+        </>
     );
 }
