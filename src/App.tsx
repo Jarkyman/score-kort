8c2f70c24ebb4ac21db8d80e83d5d5bb641e3c55
Jackie Jensen <Jackiehartvig@hotmail.com>
2026-04-05 22:38:19 +0200

    refactor: remove BetaPopup component and its reference from App layout

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,16 @@ import HomePage from "./pages/HomePage";
 import ClubListPage from "./pages/ClubListPage";
 import ClubDetailPage from "./pages/ClubDetailPage";
 import RequestPage from "./pages/RequestPage";
-import BetaPopup from "./components/BetaPopup";
 
 export default function App() {
     return (
-        <>
-            <BetaPopup />
-            <Routes>
-                <Route element={<Layout />}>
-                    <Route path="/" element={<HomePage />} />
-                    <Route path="/klubber" element={<ClubListPage />} />
-                    <Route path="/klub/:clubId" element={<ClubDetailPage />} />
-                    <Route path="/indberetning" element={<RequestPage />} />
-                </Route>
-            </Routes>
-        </>
+        <Routes>
+            <Route element={<Layout />}>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/klubber" element={<ClubListPage />} />
+                <Route path="/klub/:clubId" element={<ClubDetailPage />} />
+                <Route path="/indberetning" element={<RequestPage />} />
+            </Route>
+        </Routes>
     );
 }
